db00588e4be066c4125d0084f6c4129ad1d262b8
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-02-15 14:08:07 +0200

    feat: update the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+
 const Footer = () => {
 	return (
-		<footer
-			className={`mt-7 border-t-2 py-4 text-center text-sm font-light uppercase tracking-wide`}
-		>
-			<p>People . Technology . Seed</p>
+		<footer className={`caption_regular mt-7 border-t-2  p-4`}>
+			<div className="container flex justify-between">
+				<p>@2022 ALL rights reserved.</p>
+				<a href="https://www.linkedin.com/company/pts-holdings/">
+					<i>
+						<img
+							src="/assets/icons/linkedin-icon.svg"
+							alt="linkedin"
+							className="h-4 w-4 lg:h-6 lg:w-6"
+						/>
+					</i>
+				</a>
+			</div>
 		</footer>
 	);
 };
