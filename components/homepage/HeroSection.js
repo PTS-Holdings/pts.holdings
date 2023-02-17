2ca38897ca5bea103e9008faa67b363ec9f259a1
Omar Sakr <o.sakr@ptscorp-eg.com>
2023-02-17 15:45:25 +0200

    style(css): Adjust spacing for improved responsive

--- components/homepage/HeroSection.js
+++ components/homepage/HeroSection.js
@@ -5,12 +5,12 @@ import waveImg from "public/assets/wave.svg";
 import { HiOutlineChevronDoubleDown } from "react-icons/hi";
 const HeroSection = () => {
 	return (
-		<section className="flex flex-col-reverse items-center justify-center py-8 lg:flex-row lg:justify-between">
-			<div className="space-y-3 text-center lg:w-1/2 lg:text-left">
+		<section className="flex flex-col-reverse items-center justify-center pt-8 lg:flex-row lg:justify-between">
+			<div className="mr-8 space-y-3 text-center lg:w-1/2 lg:text-left">
 				<h2 className="title-large lg:header1 mx-auto w-[80%] font-pt font-bold text-navy-basic lg:w-full">
 					Ready to build the future now ?
 				</h2>
-				<p className="">
+				<p className="body-large-regular">
 					We are flexible, understanding that business conditions
 					change, goals change, and technology changes. We
 					continuously seek ways to develop and improve ourselves,
